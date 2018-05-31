2662232dd250365c0157a46f48413df6a5c5db92
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-31 07:35:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -27,7 +27,13 @@ function whileLoop(number) {
 }
 
 function doWhileLoop(array){
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
   
   
 }
